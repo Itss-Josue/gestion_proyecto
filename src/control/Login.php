f56d8fd7f308508880dbb988fd406ee09f0a77ea
Josue Gomez <josuegomezc21@gmail.com>
2025-11-12 12:00:49 -0500

    new

--- src/control/Login.php
+++ src/control/Login.php
@@ -6,7 +6,6 @@ require_once("../model/adminModel.php");
 
 $objUsuario = new UsuarioModel();
 $objSesion = new SessionModel();
-$objInstitucion= new InstitucionModel();
 $objAdmin = new AdminModel();
 
 $tipo = $_GET['tipo'];
@@ -35,14 +34,12 @@ if ($tipo == "iniciar_sesion") {
 
             $arrSesion = $objSesion->registrarSesion($id_usuario, $fecha_hora_inicio, $fecha_hora_fin, $llave);
             //buscamos ultimo periodo
-            $arrIes = $objInstitucion->buscarPrimerIe();
             $arrResponse = array('status' => true, 'msg' => 'Ingresar al sistema');
 
             $arr_contenido['sesion_id'] = $arrSesion;
             $arr_contenido['sesion_usuario'] = $id_usuario;
             $arr_contenido['sesion_usuario_nom'] = $arrPersona->nombres_apellidos;
             $arr_contenido['sesion_token'] = $token;
-            $arr_contenido['sesion_ies'] = $arrIes->id;
             $arrResponse['contenido'] = $arr_contenido;
         } else {
             $arrResponse = array('status' => false, 'msg' => 'Error, Usuario y/o Contraseña Incorrecta');
